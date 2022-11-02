7343e9ce9bc395505879bb9f7e556d85829c749c
Rich Davis <rdavis@galileosuite.com>
2022-11-02 13:32:16 -0400

    updates

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+
+[ -z "${1}" ] && echo "must enter token" && exit 1
+
+curl -o gpe-oracle-backup-send.sh 'https://raw.githubusercontent.com/vgcrld/gpe-tagset-oracle-backup/master/gpe-oracle-backup-send.sh'
+chmod +x gpe-oracle-backup-send.sh
+echo "${1}" > .token
+chmod 640 .token
